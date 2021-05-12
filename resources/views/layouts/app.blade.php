9136882bee939028b809a735feb449ba6a74bee4
Dora Budić <dbudic0706@gmail.com>
2021-05-12 16:48:50 +0200

    Header

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,9 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Laravel Comics</title>
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" />
     <link rel="stylesheet" href="/css/app.css">
   </head>
   <body>
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,3 +1,34 @@
 <header>
-  
+  <div class="header-top">
+    <div>
+      <p class="mr-40">dc power visa&reg;</p>
+      <p>additional dc sites<i class="fas fa-caret-down ml-5"></i></p>
+    </div>
+  </div>
+
+  <nav>
+    <div class="navbar">
+      <a href="{{route ('home')}}"><img src="img/dc-logo.png" alt="dc logo"></a>
+      <ul class="list-inline">
+        <li><a href="#">characters</a></li>
+        <li class="active"><a href="{{route ('home')}}">comics</a></li>
+        <li><a href="#">movies</a></li>
+        <li><a href="#">tv</a></li>
+        <li><a href="#">games</a></li>
+        <li><a href="#">collectibles</a></li>
+        <li><a href="#">videos</a></li>
+        <li><a href="#">fans</a></li>
+        <li><a href="#">news</a></li>
+        <li><a href="#">shop<i class="fas fa-caret-down ml-5"></i></a></li>
+      </ul>
+      <div class="search">
+        <input type="text" name="search" placeholder="Search">
+        <i class="fas fa-search ml-5"></i>
+      </div>
+    </div>
+  </nav>
+
+  <div class="jumbo">
+
+  </div>
 </header>
