a3ed14e78eaeae9afb346a377345deda5e5397e5
Dora Budić <dbudic0706@gmail.com>
2021-05-12 19:43:34 +0200

    Footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,74 @@
 <footer>
-  
+  <div class="links-container">
+    <div class="links">
+      <div class="links-lists">
+        <div class="list">
+          <div>
+            <h3>dc comics</h3>
+            <ul>
+              <li><a href="#">Characters</a></li>
+              <li><a href="#">Comics</a></li>
+              <li><a href="#">Movies</a></li>
+              <li><a href="#">TV</a></li>
+              <li><a href="#">Games</a></li>
+              <li><a href="#">Videos</a></li>
+              <li><a href="#">News</a></li>
+            </ul>
+          </div>
+          <div>
+            <h3>shop</h3>
+            <ul>
+              <li><a href="#">Shop DC</a></li>
+              <li><a href="#">Shop DC Collectibles</a></li>
+            </ul>
+          </div>
+        </div>
+        <div class="list">
+          <h3>dc</h3>
+          <ul>
+            <li><a href="#">Terms Of Use</a></li>
+            <li><a href="#">Privacy policy (New)</a></li>
+            <li><a href="#">Ad Choices</a></li>
+            <li><a href="#">Advertising</a></li>
+            <li><a href="#">Jobs</a></li>
+            <li><a href="#">Subscriptions</a></li>
+            <li><a href="#">Talent Workshops</a></li>
+            <li><a href="#">CPSC Certificates</a></li>
+            <li><a href="#">Ratings</a></li>
+            <li><a href="#">Shop Help</a></li>
+            <li><a href="#">Contact Us</a></li>
+          </ul>
+        </div>
+        <div class="list">
+          <h3>sites</h3>
+          <ul>
+            <li><a href="#">DC</a></li>
+            <li><a href="#">MAD Magazine</a></li>
+            <li><a href="#">DC Kids</a></li>
+            <li><a href="#">DC Universe</a></li>
+            <li><a href="#">DC Power Visa</a></li>
+          </ul>
+        </div>
+      </div>
+      <p>All Site Content TM and &copy; 2020 DC Entertainment, unless otherwise <a href="#">noted here</a>.
+        All rights reserved.<br>
+        <a href="#">Cookies Settings</a>
+      </p>
+    </div>
+  </div>
+  <div class="copyright">
+    <div>
+      <a href="#">sign-up now!</a>
+      <div class="social">
+        <h3>Follow us</h3>
+        <ul class="list-inline">
+          <li><a href="#"><img src="img/footer-facebook.png" alt="facebook logo"></a></li>
+          <li><a href="#"><img src="img/footer-twitter.png" alt="twitter logo"></a></li>
+          <li><a href="#"><img src="img/footer-youtube.png" alt="youtube logo"></a></li>
+          <li><a href="#"><img src="img/footer-pinterest.png" alt="pinterest logo"></a></li>
+          <li><a href="#"><img src="img/footer-periscope.png" alt="location icon"></a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
 </footer>
