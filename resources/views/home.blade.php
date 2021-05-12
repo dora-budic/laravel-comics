16e04ad390eb1130337eb611052bc245651ac11f
Dora Budić <dbudic0706@gmail.com>
2021-05-12 17:29:59 +0200

    Main - comics

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,17 @@
 @extends('layouts.app')
 
 @section('main')
-
+  <div class="comics">
+    <p>current series</p>
+    <div class="comics-container mt-30">
+      @foreach($comics as $comic)
+      <div class="comic mb-40">
+        <div class="comic-cover">
+          <img src="{{$comic['thumb']}}" alt="{{$comic['series']}} cover">
+        </div>
+        <p class="mt-20">{{$comic['series']}}</p>
+      </div>
+      @endforeach
+    </div>
+  </div>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    $database = config('comics');
+    return view('home')->with('comics',$database);
 })->name('home');
