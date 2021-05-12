1ad5e481c6eae51a5e8c0f6b5dee5535482e2147
Dora Budić <dbudic0706@gmail.com>
2021-05-12 18:40:56 +0200

    Main

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,17 +1,31 @@
 @extends('layouts.app')
 
 @section('main')
-  <div class="comics">
-    <p>current series</p>
-    <div class="comics-container mt-30">
-      @foreach($comics as $comic)
-      <div class="comic mb-40">
-        <div class="comic-cover">
-          <img src="{{$comic['thumb']}}" alt="{{$comic['series']}} cover">
+  <section id="comics-list">
+    <div class="comics">
+      <p>current series</p>
+      <div class="comics-container mt-30">
+        @foreach($comics as $comic)
+        <div class="comic mb-40">
+          <div class="comic-cover">
+            <img src="{{$comic['thumb']}}" alt="{{$comic['series']}} cover">
+          </div>
+          <p class="mt-20">{{$comic['series']}}</p>
         </div>
-        <p class="mt-20">{{$comic['series']}}</p>
+        @endforeach
       </div>
-      @endforeach
+      <a class="load" href="#">load more</a>
     </div>
-  </div>
+  </section>
+  <section id="services">
+    <div class="">
+      <ul class="list-inline">
+        <li><a href="#"><img src="img/buy-comics-digital-comics.png" alt="ebook">digital comics</a></li>
+        <li><a href="#"><img src="img/buy-comics-merchandise.png" alt="tshirt">dc merchandise</a></li>
+        <li><a href="#"><img src="img/buy-comics-subscriptions.png" alt="cards">subscription</a></li>
+        <li><a href="#"><img src="img/buy-comics-shop-locator.png" alt="location icon">comic shop locator</a></li>
+        <li><a href="#"><img src="img/buy-dc-power-visa.svg" alt="credit card">dc power visa</a></li>
+      </ul>
+    </div>
+  </section>
 @endsection
